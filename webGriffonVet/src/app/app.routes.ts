bb0bc7f31b254eab5baebefc38ff504d3f01c6e4
MNico02 <94639497+MNico02@users.noreply.github.com>
2026-04-05 23:30:23 -0300

    -agregue el template de productos

--- webGriffonVet/src/app/app.routes.ts
+++ webGriffonVet/src/app/app.routes.ts
@@ -5,57 +5,67 @@ import { roleGuard } from './guards/role-guard-guard';
 export const routes: Routes = [
   {
     path: 'login',
-    loadComponent: () => import('./pages/cliente/login/login').then(m => m.Login),
+    loadComponent: () => import('./pages/cliente/login/login').then((m) => m.Login),
   },
   {
     path: 'registro',
-    loadComponent: () => import('./pages/cliente/registro/registro').then(m => m.Registro),
+    loadComponent: () => import('./pages/cliente/registro/registro').then((m) => m.Registro),
   },
   {
     path: 'main',
-    loadComponent: () => import('./layouts/cliente/main-layout/main').then(m => m.Main),
+    loadComponent: () => import('./layouts/cliente/main-layout/main').then((m) => m.Main),
     children: [
       {
-        path: '',
-        loadComponent: () => import('./pages/cliente/home/home').then(m => m.Home),
+        path: 'home',
+        loadComponent: () => import('./pages/cliente/home/home').then((m) => m.Home),
       },
-    {
-      path: 'mis-mascotas', 
-      loadComponent: () => import('./pages/cliente/mascotas/mascotas').then(m => m.Mascotas),
-      canActivate: [authGuard, roleGuard(['CLIENTE'])],
-    }
-    ]
+      {
+        path: 'productos',
+        loadComponent: () => import('./pages/cliente/productos/productos').then((m) => m.Productos),
+      },
+      {
+        path: 'mis-mascotas',
+        loadComponent: () => import('./pages/cliente/mascotas/mascotas').then((m) => m.Mascotas),
+        canActivate: [authGuard, roleGuard(['CLIENTE'])],
+      },
+    ],
   },
   {
     path: 'admin',
-    loadComponent: () => import('./layouts/admin/admin-layout/admin-layout').then(m => m.AdminLayout),
+    loadComponent: () =>
+      import('./layouts/admin/admin-layout/admin-layout').then((m) => m.AdminLayout),
     canActivate: [authGuard, roleGuard(['ADMIN'])],
     children: [
       {
         path: '',
         redirectTo: 'dashboard',
-        pathMatch: 'full'
+        pathMatch: 'full',
       },
       {
         path: 'dashboard',
-        loadComponent: () => import('./pages/admin/dashboard-admin/dashboard-admin').then(m => m.DashboardAdmin),
+        loadComponent: () =>
+          import('./pages/admin/dashboard-admin/dashboard-admin').then((m) => m.DashboardAdmin),
         children: [
           {
             path: '',
             redirectTo: 'dashboard',
-            pathMatch: 'full'
+            pathMatch: 'full',
           },
           {
             path: 'clientes',
-            loadComponent: () => import('./pages/admin/clientes-admin/clientes-admin').then(m => m.ClientesAdmin)
-          }
-        ]
+            loadComponent: () =>
+              import('./pages/admin/clientes-admin/clientes-admin').then((m) => m.ClientesAdmin),
+          },
+        ],
       },
       {
         path: 'historialClinico/:id',
-        loadComponent: () => import('./pages/admin/historial-clinico-admin/historial-clinico-admin').then(m => m.HistorialClinicoAdmin),
-      }
-    ]
+        loadComponent: () =>
+          import('./pages/admin/historial-clinico-admin/historial-clinico-admin').then(
+            (m) => m.HistorialClinicoAdmin,
+          ),
+      },
+    ],
   },
-  { path: '**', redirectTo: '/main' }
+  { path: '**', redirectTo: '/main/home' },
 ];
